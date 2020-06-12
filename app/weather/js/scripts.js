dd1fd4c76a16c36eb447747ba9dcdcc2c409a352
Clint <cpitzak@gmail.com>
2020-06-12 10:04:07 -0700

    update

--- app/weather/js/scripts.js
+++ app/weather/js/scripts.js
@@ -1,16 +1,21 @@
-fetch("https://8ilumi4c2b.execute-api.us-west-2.amazonaws.com/room-weather/weather", {
-    mode: 'cors',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (myJson) {
-    console.log(myJson.ip);
-  })
-  .catch(function (error) {
-    console.log("Error: " + error);
-  });
+function loadWeather() {
+    fetch("https://8ilumi4c2b.execute-api.us-west-2.amazonaws.com/room-weather/weather", {
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        document.getElementById('temp-f').innerHTML = Math.round(data.temp_f * 10) / 10;
+        document.getElementById('humidity-percent').innerHTML = Math.round(data.humidity_percent * 10) / 10;
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    });
+}
+loadWeather();
+setInterval(function() {
+    loadWeather();
+}, 3000);
