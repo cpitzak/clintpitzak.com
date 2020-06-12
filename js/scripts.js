41761966dd48b3a94b15efbbef731fe409b98f83
Clint <cpitzak@gmail.com>
2020-06-12 13:40:22 -0700

    Update for when github is offline

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,10 @@ function updateRepoCount() {
         return response.json();
     })
     .then(function (data) {
+        const defaultCount = 30;
+        let count = data ? data.length : defaultCount;
+        // for when github is off line
+        count = Number.isInteger(count) || count < defaultCount ? defaultCount : count;
         document.getElementById('github-repo-count').innerHTML = data.length;
     })
     .catch(function (error) {
